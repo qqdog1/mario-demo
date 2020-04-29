0465edde9161e7ce43501610edb03f5e243ac289
qqdog1 <qqdog1@yahoo.com.tw>
2020-04-29 22:45:17 +0800

    1. tile map

--- android/assets/tileset.tsx
+++ android/assets/tileset.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.2" tiledversion="1.3.4" name="NES - Super Mario Bros - Tileset" tilewidth="16" tileheight="16" tilecount="924" columns="33">
- <image source="NES - Super Mario Bros - Tileset.png" width="528" height="448"/>
+ <image source="tileset.png" width="528" height="448"/>
 </tileset>
