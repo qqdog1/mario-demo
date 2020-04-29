457386285eeba2483f85c135c06e2f6c2ce1f4e4
Shawn <qqdog1@yahoo.com.tw>
2020-04-29 17:06:22 +0800

    1. follow the tips

--- core/src/name/qd/game/mario/MarioDemo.java
+++ core/src/name/qd/game/mario/MarioDemo.java
@@ -1,33 +1,26 @@
 package name.qd.game.mario;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class MarioDemo extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+import name.qd.game.mario.screen.PlayScreen;
+
+public class MarioDemo extends Game {
+	public SpriteBatch spriteBatch;
+
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		spriteBatch = new SpriteBatch();
+		setScreen(new PlayScreen(this));
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		super.render();
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
+		spriteBatch.dispose();
 	}
 }
---
+++ core/src/name/qd/game/mario/screen/PlayScreen.java
@@ -0,0 +1,57 @@
+package name.qd.game.mario.screen;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+
+import name.qd.game.mario.MarioDemo;
+
+public class PlayScreen implements Screen {
+    private MarioDemo game;
+    private Texture texture;
+
+    public PlayScreen(MarioDemo game) {
+        this.game = game;
+        texture = new Texture("badlogic.jpg");
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        game.spriteBatch.begin();
+        game.spriteBatch.draw(texture, 0, 0);
+        game.spriteBatch.end();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
