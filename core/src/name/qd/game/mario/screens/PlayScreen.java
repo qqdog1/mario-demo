c221601c1dac80d798380911a0266e245029886d
Shawn <qqdog1@yahoo.com.tw>
2020-04-30 16:07:19 +0800

    1. box2d sharp

--- core/src/name/qd/game/mario/screens/PlayScreen.java
+++ core/src/name/qd/game/mario/screens/PlayScreen.java
@@ -4,10 +4,20 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
@@ -24,6 +34,9 @@ public class PlayScreen implements Screen {
     private TiledMap map;
     private OrthogonalTiledMapRenderer mapRenderer;
 
+    private World world;
+    private Box2DDebugRenderer box2DDebugRenderer;
+
     public PlayScreen(MarioDemo game) {
         this.game = game;
         camera = new OrthographicCamera();
@@ -35,6 +48,33 @@ public class PlayScreen implements Screen {
         mapRenderer = new OrthogonalTiledMapRenderer(map);
 
         camera.position.set(viewport.getScreenWidth() / 2, viewport.getScreenHeight() / 2, 0);
+
+        world = new World(new Vector2(0, 0), true);
+        box2DDebugRenderer = new Box2DDebugRenderer();
+
+        BodyDef bodyDef = new BodyDef();
+        PolygonShape polygonShape = new PolygonShape();
+        FixtureDef fixtureDef = new FixtureDef();
+        bodyDef.type = BodyDef.BodyType.StaticBody;
+
+        setFixture(map.getLayers().get("ground").getObjects().getByType(RectangleMapObject.class), bodyDef, polygonShape, fixtureDef);
+        setFixture(map.getLayers().get("pipes").getObjects().getByType(RectangleMapObject.class), bodyDef, polygonShape, fixtureDef);
+        setFixture(map.getLayers().get("coins").getObjects().getByType(RectangleMapObject.class), bodyDef, polygonShape, fixtureDef);
+        setFixture(map.getLayers().get("bricks").getObjects().getByType(RectangleMapObject.class), bodyDef, polygonShape, fixtureDef);
+    }
+
+    private void setFixture(Array<RectangleMapObject> array, BodyDef bodyDef, PolygonShape polygonShape, FixtureDef fixtureDef) {
+        Body body;
+        for(MapObject mapObject : array) {
+            Rectangle rectangle = ((RectangleMapObject) mapObject).getRectangle();
+            bodyDef.position.set(rectangle.getX() + (rectangle.getWidth() / 2), rectangle.getY() + (rectangle.getHeight() / 2));
+
+            body = world.createBody(bodyDef);
+
+            polygonShape.setAsBox(rectangle.getWidth() / 2, rectangle.getHeight() / 2);
+            fixtureDef.shape = polygonShape;
+            body.createFixture(fixtureDef);
+        }
     }
 
     @Override
@@ -63,6 +103,8 @@ public class PlayScreen implements Screen {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         mapRenderer.render();
+
+        box2DDebugRenderer.render(world, camera.combined);
 
         game.spriteBatch.setProjectionMatrix(hud.stage.getCamera().combined);
         hud.stage.draw();
